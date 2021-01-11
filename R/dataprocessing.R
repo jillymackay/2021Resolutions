8ea7ddee2aa97ffd80400c155397dc49f1f06340
jillymackay <jill.mackay@ed.ac.uk>
2021-01-11 22:40:49 +0000

    chart tweaks

--- R/dataprocessing.R
+++ R/dataprocessing.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(googlesheets)
+library(nord)
 
 
 
@@ -20,6 +21,12 @@ dat <- dat %>%
 
 
 dat %>% 
-  ggplot(aes(x = day, y = word_count, shape = x30_minutes, colour = genre)) +
-  geom_point() +
-  theme_classic()
+  ggplot(aes(x = day, y = word_count, fill = x30_minutes, shape = genre)) +
+  geom_bar(stat = "identity") +
+  theme_classic() +
+  labs(x = "Date", y = "Word Count") +
+  scale_fill_nord(palette = "halifax_harbor")
+
+
+
+# make a new palette with hue and luminence combined for genre and time?
